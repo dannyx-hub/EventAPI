01bb931e591a1d7a78b5b98e59a80fa02adec863
dannyx-hub <54348467+dannyx-hub@users.noreply.github.com>
2022-01-09 14:47:41 +0100

    naprawa mojej glupoty

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def ping_pong():
 
 
 if __name__ == '__main__':
-try:
-    app.run()
-except Exception as e:
-    logging.error(e)
+    try:
+        app.run()
+    except Exception as e:
+        logging.error(e)
