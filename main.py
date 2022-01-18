a02efb77376215b8d15f777c00908a650c606106
dannyx-hub <daro322.dp@gmail.com>
2022-01-18 19:04:20 +0100

    some changes in database, update test.html

--- main.py
+++ main.py
@@ -70,8 +70,8 @@ def lecturesadd():
 @app.route('/api/list',methods=['GET'])
 def list():
     jsonobj = []
-    columns = ["eventname","eventdate","eventpersoncreator"]
-    list = db.CursorExec('SELECT eventname,eventdate,eventpersoncreator from events')
+    columns = ["eventname","eventstartdate","eventstopdata","eventpersoncreator"]
+    list = db.CursorExec('SELECT eventname,eventstartdate,eventstopdata,eventpersoncreator from events where approved = True')
     for x in range(len(list)):
        data={}
        for col in range(len(columns)):
