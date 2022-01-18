225a7939f18baf019071a9f62df1334169359ff9
dannyx-hub <daro322.dp@gmail.com>
2022-01-18 21:41:00 +0100

    update sql

--- Event.sql
+++ Event.sql
@@ -2,3 +2,5 @@ CREATE TABLE IF NOT EXISTS events(id SERIAL PRIMARY KEY,eventname TEXT NOT NULL,
 CREATE TABLE IF NOT EXISTS users(id SERIAL NOT NULL PRIMARY KEY,login TEXT NOT NULL,hash TEXT NOT NULL,role VARCHAR(10) NOT NULL);
 Insert into events(eventname,eventdate,eventpersoncreator) VALUES('test3','2022-01-01','Damian');
 Insert into users(login,hash,role) values('root','63a9f0ea7bb98050796b649e85481845','root');
+ALTER TABLE events ADD COLUMN approved boolean not null;
+ALTER TABLE events ADD COLUMN eventstopdate TIMESTAMP;
